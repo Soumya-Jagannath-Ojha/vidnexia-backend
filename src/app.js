0c2c4ebec04de2044188d1c8c1114845fade31f3
Soumya-Jagannath-Ojha <soumyajagannathojha7@gmail.com>
2026-01-04 07:36:28 +0530

    connect to database

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors"
+
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    })
+);
+
+// express common middleware
+app.use(express.json({limit: "16kb"}))    // to recieve file
+app.use(express.urlencoded({extended: true, limit: "16kb"}))   //EX: url space percentage
+app.use(express.static("public"))   //Static files
+
+
+export {app};
